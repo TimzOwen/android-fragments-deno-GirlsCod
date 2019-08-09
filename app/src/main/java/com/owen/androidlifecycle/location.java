843431351ad07fc91987ef7e16d88da87783eb30
Timz Owen <ktimothy@kabarak.ac.ke>
2019-08-09 10:05:38 -0700

    Update location.java
    updated the adress to get exact location

--- app/src/main/java/com/owen/androidlifecycle/location.java
+++ app/src/main/java/com/owen/androidlifecycle/location.java
@@ -83,8 +83,30 @@ try {
            ", Longitude = " +
            location.getLongitude(), illegalArgumentException);
 }
-                   
-                   
+                  
+                   if (addresses == null || addresses.size() == 0) {
+   if (resultMessage.isEmpty()) {
+       resultMessage = mContext
+               .getString(R.string.no_address_found);
+       Log.e(TAG, resultMessage);
+   }
+}
+                     else {
+       // If an address is found, read it into resultMessage
+       Address address = addresses.get(0);
+       ArrayList<String> addressParts = new ArrayList<>();
+
+       // Fetch the address lines using getAddressLine,
+       // join them, and send them to the thread
+       for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+           addressParts.add(address.getAddressLine(i));
+       }
+
+       resultMessage = TextUtils.join("\n", addressParts);
+    }
+                   nterface OnTaskCompleted {
+        void onTaskCompleted(String result);
+}
                    
                    
                    
